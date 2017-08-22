084c4fc9bfadbc17f6ba2664ede945e565ec10c7
unknown <patrickmartinez77@gmail.com>
2017-08-21 21:34:04 -0500

    Adding js button functions

--- js/script.js
+++ js/script.js
@@ -2,51 +2,20 @@
 /*jslint devel: true */
 /*global $, jQuery*/
 
+/*API URL: /w/api.php?action=query&format=json&prop=info%7Cextracts&generator=search&callback=&inprop=url&exsentences=4&exintro=1&gsrsearch=Bacon&gsrlimit=20 */
+
+
 $(document).ready(function () {
     "use strict";
     
-    var lat, lon, weather, icon, desc, city, tempC, tempF, country, tempUnits;
+    $("#random-button").on("click", function () {
+        window.open("https://en.wikipedia.org/wiki/Special:Random", '_blank');
+    });
     
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(getWeather);
-        } else {
-            $("#title").html("Can't get location.");
-        }
-    }
-    
-    function getWeather(position) {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon,
-            function getDetails(json) {
-                city = json.name;
-                country = json.sys.country;
-                icon = json.weather[0].icon;
-                desc = json.weather[0].main;
-                tempC = Math.round(json.main.temp);
-                tempF = Math.round(tempC * 1.8 + 32);
-            });
-        $(document).ajaxComplete(function () {
-            $("#title").html(city + ", " + country);
-            $("#icon-box").css("backgroundImage", "url(" + icon + ")");
-            $("#description").html(desc);
-            tempUnits = 'F';
-            $("#temp").html(tempF + '&degF');
-        });
-    }
-    
-    $("#degree-change").on("click", function () {
-        if (tempUnits === 'F') {
-            $("#temp").html(tempC + '&degC');
-            $("#degree-change").html("To fahrenheit");
-            tempUnits = 'C';
-        } else {
-            $("#temp").html(tempF + '&degF');
-            $("#degree-change").html("To celcius");
-            tempUnits = 'F';
-        }
-            
-        $("#temp").html();
+    $("#search-button").on("click", function () {
+        var input;
+        input = $("#search-box").val();
+        $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&titles=" + input + "&");
+
     });
 });
